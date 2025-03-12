e5042a54f851a4bab47a02232cb58beb44a38765
czajkub <czajkub@gmail.com>
2025-03-12 21:13:12 +0100

    started work on js

---
+++ parser.js
@@ -0,0 +1,13 @@
+document.getElementById("submitfile").onclick = function () {
+    const ifile = document.getElementById("inputfile");
+    if(ifile == null) {
+        document.getElementById("submitpaste").innerHTML = "Please enter a file";
+        return;
+    }
+}
+document.getElementById("submiturl").onclick = function () {
+
+}
+document.getElementById("submitpaste").onclick = function () {
+
+}
